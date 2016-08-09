6584e19b9ddacbbb2458c182fe722ba03b63ba42
Jessica <jessicaraepetersen@gmail.com>
2016-08-09 21:39:56 +0000

    consolidated scope & auth process, added get followed artist list function

--- test2.py
+++ test2.py
@@ -1,8 +1,11 @@
 import sys
 import spotipy
 import spotipy.util as util
+from math import ceil
 
-scope = 'user-library-read'
+scope = '''user-follow-read, playlist-read-private, playlist-read-collaborative, 
+            playlist-modify-public, playlist-modify-private'''
+
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -10,17 +13,17 @@ else:
     print "Usage: %s username" % (sys.argv[0],)
     sys.exit()
 
-token = util.prompt_for_user_token(username, scope)
+token = util.prompt_for_user_token(username)
 
 #Gets the first 20 album ids from the Spotify new release list
 if token:
     print "The token exists!"
     sp = spotipy.Spotify(auth=token)
-    new_release_results = sp.new_releases(limit=20)
+    new_release_results = sp.new_releases(limit=20, offset=)
 
     total_available = new_release_results['albums']['total']
-    num_results = new_release_results['albums']['limit']
-    times_to_offset = (total_available/num_results) - 1
+    num_results = len(new_release_results['albums']['items'])
+    times_to_offset = int(ceil(total_available/float(num_results)) - 1)
 
     new_release_album_ids = []
     for i in range(num_results):
@@ -37,7 +40,7 @@ if token:
     album_results = sp.albums(new_release_album_ids)
 
     new_release_artist_ids = []
-    for i in range(20):
+    for i in range(len(album_results['albums'])):
         new_release_artist_ids.append(str(album_results['albums'][i]['artists'][0]['id']))
         # album_ids_joined = ','.join(new_release_album_ids)
 else:
@@ -45,23 +48,36 @@ else:
 
 #Gets the first 50 artists a user follows
 
-# scope = 'user-follow-read'
-# token = util.prompt_for_user_token(username, scope)
+if token:
+    print "The token exists!"
+    sp = spotipy.Spotify(auth=token)
+    first_artist_followed_results = sp.current_user_followed_artists(limit=50)
 
-# if token:
-#     print "The token exists!"
-#     sp = spotipy.Spotify(auth=token)
-#     album_results = sp.current_user_followed_artists(limit=50)
+    total_artists_user_follows = float(first_artist_followed_results['artists']['total'])
+    num_results = len(first_artist_followed_results['artists']['items'])
+    num_to_loop = int(ceil(total_artists_user_follows / float(num_results)) - 1)
 
-#     total_available = new_release_results['artists']['items']['total']
-#     num_results = new_release_results['artists']['limit']
+    user_followed_artists_ids = []
+    for i in range(num_results):
+        user_followed_artists_ids.append(str(first_artist_followed_results['artists']['items'][i]['id']))
 
-#     # new_release_artist_ids = []
-#     # for i in range(20):
-#     #     new_release_artist_ids.append(str(album_results['albums'][i]['artists'][0]['id']))
-#     #     # album_ids_joined = ','.join(new_release_album_ids)
-# else:
-#     print "Can't get token for", username
+        # album_ids_joined = ','.join(new_release_album_ids)
+else:
+    print "Can't get token for", username
+
+#Gets the rest of the artist ids that the user follows
+def fill_user_follows():
+    for i in range(num_to_loop):
+        if token:
+            print "The token exists!"
+            sp = spotipy.Spotify(auth=token)
+            artists_user_follows_results = sp.current_user_followed_artists(limit=50, 
+                                                    after=user_followed_artists_ids[-1])
+
+            for i in range(len(artists_user_follows_results['artists']['items'])):
+                user_followed_artists_ids.append(str(artists_user_follows_results['artists']['items'][i]['id']))
+
+fill_user_follows()
 
 
 
--- test3.py
+++ test3.py
@@ -1,6 +1,8 @@
 import sys
 import spotipy
 import spotipy.util as util
+from math import ceil
+import test2
 
 scope = 'user-follow-read'
 
@@ -15,14 +17,46 @@ token = util.prompt_for_user_token(username, scope)
 if token:
     print "The token exists!"
     sp = spotipy.Spotify(auth=token)
-    album_results = sp.current_user_followed_artists(limit=50)
+    first_artist_followed_results = sp.current_user_followed_artists(limit=50)
 
-    # total_available = new_release_results['artists']['items']['total']
-    # num_results = new_release_results['artists']['limit']
+    total_artists_user_follows = float(first_artist_followed_results['artists']['total'])
+    num_results = float(first_artist_followed_results['artists']['limit'])
+    num_to_loop = int(ceil(total_artists_user_follows / num_results) - 1)
 
-    # new_release_artist_ids = []
-    # for i in range(20):
-    #     new_release_artist_ids.append(str(album_results['albums'][i]['artists'][0]['id']))
-    #     # album_ids_joined = ','.join(new_release_album_ids)
+    user_followed_artists_ids = []
+    for i in range(len(first_artist_followed_results['artists']['items'])):
+        user_followed_artists_ids.append(str(first_artist_followed_results['artists']['items'][i]['id']))
+
+        # album_ids_joined = ','.join(new_release_album_ids)
 else:
     print "Can't get token for", username
+
+def fill_user_follows():
+    for i in range(num_to_loop):
+        if token:
+            print "The token exists!"
+            sp = spotipy.Spotify(auth=token)
+            artists_user_follows_results = sp.current_user_followed_artists(limit=50, 
+                                                    after=user_followed_artists_ids[-1])
+
+            for i in range(len(artists_user_follows_results['artists']['items'])):
+                user_followed_artists_ids.append(str(artists_user_follows_results['artists']['items'][i]['id']))
+
+fill_user_follows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
