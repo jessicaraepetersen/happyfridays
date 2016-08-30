f19f078757ee1090bac4602e573c7d80f53fb9c5
Jessica <jessicaraepetersen@gmail.com>
2016-08-30 01:18:15 +0000

    updated port

--- server.py
+++ server.py
@@ -153,4 +153,6 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
 
-    app.run(host="0.0.0.0", port=5000)
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=PORT)
